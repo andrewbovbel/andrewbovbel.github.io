5e28dcaf0a9f71f4c86658ee015081c77f99d95f
Andrew Bovbel <andrew@bovbel.com>
2023-09-06 21:24:48 -0400

    feat: added terminal

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+  
+//     <App />
+//   </BrowserRouter>,
+// );
+
+ReactDOM.render(
+  <React.StrictMode>
+    <BrowserRouter>
+    <App /></BrowserRouter>
+    
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,87 @@
+import styled from 'styled-components';
+import React, { useState } from 'react';
+
+const Terminal = styled.div`
+    display: flex;
+    align-items: center;
+    background-color: black;
+    border: 1px solid white;
+    padding: 10px;
+    margin: 10px;
+`
+
+const Prompt = styled.div`
+    color: green;
+    margin-right: 10px;
+`
+
+const Input = styled.input`
+    background-color: black;
+    color: white;
+    border: none;
+    font-family: monospace;
+    outline: none;
+    width: 100%;
+`
+
+const Output = styled.div`
+    margin: 10px;
+    padding: 10px;
+    border: 1px solid white;
+    background-color: black;
+    white-space: pre-wrap;
+    max-height: 200px;
+    overflow-y: auto;
+`
 function Home() {
-    return <div>Hi</div>
+
+    const [input, setInput ] = useState('');
+    const [output, setOutput] = useState([]);
+    const help = ["`clear` will clear the output", "`off` is currently in development"]
+
+    function handleInputChange(e) {
+        setInput(e.target.value)
+    }
+
+    function handleInputSubmit(e) {
+        const {key, target: {value} } = e;
+
+        if (key === 'Enter') {
+            console.log(value)
+            setOutput([...output, value])
+            setInput('')
+
+            if (value.toLowerCase() == 'clear') {
+                setOutput([])
+            }
+
+            if (value.toLowerCase() == 'help') {
+                setOutput([...output, ...help])
+            }
+             
+            if (value.toLowerCase() == "off") {
+                setOutput([...output, "`off` is currently in development, sorry!"])
+            }
+        }
+    }
+
+    return(
+        <>
+        <Output>
+        Welcome to my website! Type `help` if you would like to understand how to use this terminal style website, or `off` to switch back to a GUI
+        </Output>
+        <Output>
+            {output.map((line, index) => (
+                <div key={index}>{line}</div>
+            ))}
+        </Output>
+        <Terminal>
+            <Prompt>user@andrewbovbelresume</Prompt>
+            <Input onKeyDown={handleInputSubmit} value={input} onChange={handleInputChange}></Input>
+        </Terminal>
+        
+         </>
+    )
 }
 
 export default Home;
